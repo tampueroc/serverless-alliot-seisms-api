d6528456dbcfa03bf2b4ff1b4e9fadb74b834dfc
Tomás Ampuero <46233260+tampueroc@users.noreply.github.com>
2023-08-22 10:13:48 -0400

    fix: linting

--- src/seisms_handler.py
+++ src/seisms_handler.py
@@ -3,7 +3,6 @@
 import datetime
 import json
 import logging
-import os
 
 from .common.s3_client import S3Client
 
@@ -19,13 +18,17 @@ def get_seisms(event: dict, context: dict):
         s3_client = S3Client()
         logging.info(f"Query string parameters to sql query: {query_string_parameters.to_sql_query()}")
         s3_response = s3_client.get_files('seisms-bucket', query_string_parameters.to_sql_query())
+        return {
+            'statusCode': 200,
+            'body': json.dumps(s3_response['Payload'].read().decode('utf-8'))
+        }
     except Exception as e:
         print(e)
         return {
             'statusCode': 500,
             'body': json.dumps('Internal Server Error')
         }
-    
+
 
 def create_seisms(event: dict, context: dict):
     try:
@@ -34,12 +37,12 @@ def create_seisms(event: dict, context: dict):
         seisms_entries = [GetSeismPayload(**seism) for seism in event_body]
         s3_client = S3Client()
         # Create folder for tmp files
-        with open(f"seisms.csv", "w") as f:
+        with open("seisms.csv", "w") as f:
             f.write('timestamp, country, magnitude\n')
             for seism in seisms_entries:
                 f.write(f"{seism.timestamp}, {seism.country}, {seism.magnitude}")
                 f.write('\n')
-            s3_response = s3_client.upload_file(f.name, 'seisms-bucket', f.name)
+            s3_client.upload_file(f.name, 'seisms-bucket', f.name)
             f.close()
     except Exception as e:
         print(e)
