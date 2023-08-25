6237d2c85df8f2fc39aa2b5815e2964780d511f4
Tomás Ampuero <46233260+tampueroc@users.noreply.github.com>
2023-08-25 09:57:24 -0400

    fix: remove json dumps from applicaiton logic

--- src/common/utils.py
+++ src/common/utils.py
@@ -4,5 +4,5 @@ import json
 def create_http_response(status_code: int, body: str) -> dict:
     return {
         'statusCode': status_code,
-        'body': json.dumps(body)
+        'body': body
     }
