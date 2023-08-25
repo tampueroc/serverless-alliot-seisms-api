e8538b7dca3eac25a5da3f79ebebb1a2538d9bc5
Tomás Ampuero <46233260+tampueroc@users.noreply.github.com>
2023-08-24 22:21:03 -0400

    fix: decode body

--- src/seisms_handler.py
+++ src/seisms_handler.py
@@ -38,7 +38,7 @@ def create_seisms(event: dict, context: dict):
         filename = '/tmp/seisms.csv'
         seism_file = s3_client.get_file_by_key('seisms-bucket', 'seisms.csv')
         if seism_file:
-            logging.info(f"Seism file: {seism_file['Payload'].get('Body').decode('utf-8')}")
+            logging.info(f"Seism file: {seism_file['Body'].read().decode('utf-8')}")
             pass
         with open(filename, "w") as f:
             f.write('timestamp,country,magnitude\n')
