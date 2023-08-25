db7231bf5af96195ba89af4c0032329b5a4b6fde
Tomás Ampuero <46233260+tampueroc@users.noreply.github.com>
2023-08-25 10:17:22 -0400

    fix: cls json encoder

--- src/seisms_handler.py
+++ src/seisms_handler.py
@@ -24,7 +24,7 @@ def get_seisms(event: dict, context: dict):
             return create_http_response(400, 'Bad Request: Too many entries, more than 100')
         entries = [SeismEntry(**entry) for entry in s3_response]
         entries.sort(key=lambda x: x.timestamp)
-        return create_http_response(200, json.dumps(entries, default=CustomPydanticJSONEncoder))
+        return create_http_response(200, json.dumps(entries, cls=CustomPydanticJSONEncoder))
     except Exception as e:
         logging.exception(f"Exception in get_seisms lambda function: {e}")
         return create_http_response(500, 'Internal Server Error')
